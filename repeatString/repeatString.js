5052eb8b41e45762db4fedf8492b998cc127e8b3
Frd0mF <ibrahimbouslime@gmail.com>
2021-03-27 03:10:05 +0100

    exercice 2

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeat = '';
+    if (num == 0) {
+        repeat = '';
+    } else if (num < 0) {
+        repeat = 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+            repeat += string;
+        }
+    }
+    return repeat;
 }
 
 module.exports = repeatString
