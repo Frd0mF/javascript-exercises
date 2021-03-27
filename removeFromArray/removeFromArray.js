43ffb2de2c730e72d62d1b1c8f1dd18a5a2755ab
Frd0mF <ibrahimbouslime@gmail.com>
2021-03-27 16:12:57 +0100

    exercise3

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, ...item) {
+    for (let i = 0; i < item.length; i++) {
+        for (let j = 0; j < list.length; j++) {
+            if (item[i] === list[j]) {
+                list.splice(j, 1)
+            }
+        }
+    }
+    return list
 }
+
 
 module.exports = removeFromArray
