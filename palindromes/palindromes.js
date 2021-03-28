84c92f8c935d14fb8f8dd7b439e50999786cde66
Frd0mF <ibrahimbouslime@gmail.com>
2021-03-28 23:12:45 +0100

    exercice

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
-
+const palindromes = function(string) {
+    string = string.replace(/[&\/\\#,+()$~%.'":*?!<>{} ]/g, '');
+    string = string.toLowerCase();
+    stringSplit = string.split("");
+    let palindrome = '';
+    for (let i = string.length; i > 0; i--) {
+        palindrome += string[i - 1];
+    }
+    if (palindrome == string) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = palindromes
