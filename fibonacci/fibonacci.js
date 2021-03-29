5b2a4605ef00602b347d30700f7fb54bd5b02ebc
Frd0mF <ibrahimbouslime@gmail.com>
2021-03-29 21:50:16 +0100

    exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    if (fib < 1) {
+        return 'OOPS'
+    } else if (fib == 1) {
+        return 1
+    } else {
+        var a = 1
+        var b = 1;
+        let temp = 0;
+        for (let i = 1; i < fib; i++) {
+            temp = a;
+            a = b;
+            b = temp + b;
+        }
+    }
+    return a
 }
 
 module.exports = fibonacci
