bd71c21602c341b3959fbcab95908aaa9726dc25
Frd0mF <ibrahimbouslime@gmail.com>
2021-03-29 21:57:44 +0100

    exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,13 +1,13 @@
 const fibonacci = function(fib) {
-    if (fib < 1) {
+    if (+fib < 1) {
         return 'OOPS'
-    } else if (fib == 1) {
+    } else if (+fib == 1) {
         return 1
     } else {
         var a = 1
         var b = 1;
         let temp = 0;
-        for (let i = 1; i < fib; i++) {
+        for (let i = 1; i < +fib; i++) {
             temp = a;
             a = b;
             b = temp + b;
