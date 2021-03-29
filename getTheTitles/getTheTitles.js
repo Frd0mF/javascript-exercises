cfced0a454f43355a13d1b94c1578cdebba2c0d3
Frd0mF <ibrahimbouslime@gmail.com>
2021-03-29 22:14:54 +0100

    exercise

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    var array = []
+    for (let i = 0; i < arr.length; i++) {
+        array.push(arr[i].title)
+    }
+    return array
 }
-
 module.exports = getTheTitles;
